0d03e7059efdff022864a61e6e9bffc545c39a23
Dzmitry Bodry <megabodry@gmail.com>
2024-08-27 16:42:17 +0300

    Completed task 5.

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height; j++) {
+                if ((j < i || j > height - i + 1) && i <= height / 2) {
+                    System.out.print(" ");
+                } else if ((j > i || j < height - i + 1) && i > height / 2)  {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(8);
+                }
+            }
+            System.out.println();
+        }
     }
 }
